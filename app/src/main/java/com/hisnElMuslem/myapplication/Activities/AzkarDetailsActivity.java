3618d058a2cae3d52ef6295b7786368456a7caab
hassan20105 <hassanhassanragheb@outlook.com>
2020-03-23 18:04:50 +0200

    PushApp2

--- app/src/main/java/com/hisnElMuslem/myapplication/Activities/AzkarDetailsActivity.java
+++ app/src/main/java/com/hisnElMuslem/myapplication/Activities/AzkarDetailsActivity.java
@@ -1,14 +1,12 @@
 package com.hisnElMuslem.myapplication.Activities;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
-
 import android.os.Bundle;
 import android.view.Window;
 import android.view.WindowManager;
-import android.widget.FrameLayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import com.hisnElMuslem.myapplication.Fragments.DynamicFragment;
 import com.hisnElMuslem.myapplication.R;
