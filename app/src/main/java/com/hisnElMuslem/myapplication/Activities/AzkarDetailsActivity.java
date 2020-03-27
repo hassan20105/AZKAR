5207fb1480837261dd4448d46124337224ba1123
hassan20105 <hassanhassanragheb@outlook.com>
2020-03-27 22:53:05 +0200

    Update Design

--- app/src/main/java/com/hisnElMuslem/myapplication/Activities/AzkarDetailsActivity.java
+++ app/src/main/java/com/hisnElMuslem/myapplication/Activities/AzkarDetailsActivity.java
@@ -5,18 +5,12 @@ import android.view.Window;
 import android.view.WindowManager;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
 import androidx.viewpager.widget.ViewPager;
 
-import com.hisnElMuslem.myapplication.Fragments.DynamicFragment;
 import com.hisnElMuslem.myapplication.Fragments.FragmentCollectionAdapter;
-import com.hisnElMuslem.myapplication.Model.ZekrItems;
 import com.hisnElMuslem.myapplication.R;
-import com.hisnElMuslem.myapplication.Services.Databases.AzkarDBServices;
 
 import java.util.ArrayList;
-import java.util.List;
 
 public class AzkarDetailsActivity extends AppCompatActivity {
     private ViewPager viewPager;
@@ -27,7 +21,6 @@ public class AzkarDetailsActivity extends AppCompatActivity {
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_azkar_details);
-        AzkarDBServices azkarDBServices = new AzkarDBServices(this);
         ArrayList<String> zekrItems = getIntent().getStringArrayListExtra("azkarList");
         viewPager = findViewById(R.id.view_pager);
         adapter = new FragmentCollectionAdapter(getSupportFragmentManager(), zekrItems);
@@ -36,16 +29,4 @@ public class AzkarDetailsActivity extends AppCompatActivity {
 
     }
 
-   /* private void addFragement()
-    {
-        FragmentManager fragmentManager = getSupportFragmentManager();
-        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-        fragmentTransaction.setCustomAnimations(R.anim.slide_left_in,R.anim.slide_left_out,R.anim.slide_right_in,R.anim.slide_right_out);
-       overridePendingTransition(R.anim.slide_left_in,R.anim.slide_right_in);
-        DynamicFragment dynamicFragment = new DynamicFragment();
-        fragmentTransaction.replace(R.id.dynamicFrame,dynamicFragment);
-        fragmentTransaction.commit();
-
-
-    }*/
 }
